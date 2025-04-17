fb056a994ca59b02a85b743b452c14222e1d5e4c
Nicholas Clark <nicholas.j.clark1214@gmail.com>
2025-04-17 19:22:21 +1000

    flexhing out the forecast utilities

--- forecasting_ffc_gam.R
+++ forecasting_ffc_gam.R
@@ -30,7 +30,7 @@ functional_fc
 
 object = mod
 newdata = qld_test
-n_draws = 10
+n_draws = 5
 n_sims = 10
 model = ETS()
 
@@ -41,6 +41,7 @@ orig_lpmat <- predict(
   type = 'lpmatrix'
 )
 
+# Take full draws of beta coefficients
 orig_betas <- mgcv::rmvn(
   n = n_draws * n_sims,
   mu = coef(object),
@@ -51,8 +52,17 @@ if(is.null(object$gam_init)) {
 
   # No need to modify lpmatrix if there were no
   # fts() terms in the model
-  full_lpmat <- orig_lpmat
-  full_betas <- orig_betas
+  full_linpreds <- matrix(
+    as.vector(
+      t(apply(
+        as.matrix(orig_betas),
+        1,
+        function(row) orig_lpmat %*% row +
+          attr(orig_lpmat, 'model.offset')
+      ))
+    ),
+    nrow = NROW(orig_betas)
+  )
 
 } else {
   # Determine horizon (assuming equal time gaps)
@@ -91,7 +101,7 @@ if(is.null(object$gam_init)) {
     )
   }
 
-  # Fit time series model to the basis coefficients
+  # Fit the time series model to the basis coefficients
   # and generate forecasts
   functional_fc <- forecast(
     object = functional_coefs,
@@ -99,6 +109,13 @@ if(is.null(object$gam_init)) {
     times = n_sims,
     model = mod_name
   )
+
+  # Only need to return forecasts for those times that are
+  # in newdata
+  functional_fc <- functional_fc %>%
+    dplyr::filter(
+      .time %in% unique(interpreted$data[[time_var]])
+    )
 
   # Which coefficients in lpmatrix are associated with fts objects?
   smooth_names <- unlist(
@@ -120,17 +137,21 @@ if(is.null(object$gam_init)) {
     use.names = FALSE
   )
 
-  # Drop these columns from the lpmatrix
+  # Drop these columns from the lpmatrix and the betas
   intermed_lpmat <- orig_lpmat[, -fts_coefs, drop = FALSE]
+  intermed_betas <- orig_betas[, -fts_coefs, drop = FALSE]
 
-  # Add columns of 1s for the fts basis coefficients
-  full_lpmat <- cbind(
-    intermed_lpmat,
-    matrix(1,
-           nrow = NROW(intermed_lpmat),
-           ncol = NCOL(interpreted$data[grep('fts_',
-                                             colnames(interpreted$data))])
-    )
+  # Calculate intermediate linear predictors
+  intermed_linpreds <- matrix(
+    as.vector(
+      t(apply(
+        as.matrix(intermed_betas),
+        1,
+        function(row) intermed_lpmat %*% row +
+          attr(orig_lpmat, 'model.offset')
+      ))
+    ),
+    nrow = NROW(intermed_betas)
   )
 
   # Join forecasts to the basis function evaluations
@@ -165,10 +186,48 @@ if(is.null(object$gam_init)) {
     tidyr::pivot_wider(
       names_from = .basis,
       values_from = .pred
-    )
+    ) %>%
+    dplyr::arrange(.row) %>%
+    dplyr::mutate(.draw = paste0(.realisation, '_', .rep)) %>%
+    dplyr::select(-.time,
+                  -.realisation,
+                  -.rep)
 
   # Should now have n_draws * n_sims draws for each row of newdata
   if(!NROW(full_lpmat) * n_draws * n_sims == NROW(fts_fc)){
     stop('Wrong dimensions in forecast coefs; need to check on this')
   }
+
+  # Should also have same ncols as number of fts basis functions
+  if(
+    !NCOL(fts_fc) - 2 ==
+    NCOL(interpreted$data[
+      grep('fts_',
+           colnames(interpreted$data))])
+  ){
+    stop('Wrong dimensions in forecast coefs; need to check on this')
+  }
+
+  # If dimensions correct, take rowsums for each draw
+  fc_linpreds <- fts_fc %>%
+    dplyr::select(-c(.draw, .row)) %>%
+    rowSums()
+
+  # Add the draw-specific row predictions to the
+  # intermediate prediction matrix
+  unique_draws <- unique(fts_fc$.draw)
+  full_linpreds <- do.call(
+    rbind,
+    lapply(
+      unique_draws,
+      function(x){
+        fc_linpreds[which(fts_fc$.draw == x)]
+      }
+    )
+  ) +
+    intermed_linpreds
 }
+
+# Now can proceed to send full_linpreds to the relevant
+# invlink and rng functions for outcome-level predictions;
+# use gratia::posterior_samples() codebase as a guide
